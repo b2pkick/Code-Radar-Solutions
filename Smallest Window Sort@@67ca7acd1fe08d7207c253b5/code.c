e298443accee81a3cae316785431e24d7f9b9513
Rahul Gupta <rahulgupta79777@gmail.com>
2025-03-19 17:54:53 +0530

    Solution to Smallest Window Sort

--- Smallest Window Sort@@67ca7acd1fe08d7207c253b5/code.c
+++ Smallest Window Sort@@67ca7acd1fe08d7207c253b5/code.c
@@ -1,20 +1,37 @@
-void selectionSort(int arr[], int n) {
+#include <stdio.h>
+
+int findUnsortedSubarray(int arr[], int n) {
+    int L = -1, R = -1;
     for (int i = 0; i < n - 1; i++) {
-        int minIndex = i;
-        for (int j = i + 1; j < n; j++) {
-            if (arr[j] < arr[minIndex]) {
-                minIndex = j;
-            }
+        if (arr[i] > arr[i + 1]) {
+            L = i;
+            break;
         }
-       
-        int temp = arr[i];
-        arr[i] = arr[minIndex];
-        arr[minIndex] = temp;
     }
-}
-void printArray(int arr[], int n) {
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
+    if (L == -1) return 0;
+    for (int i = n - 1; i > 0; i--) {
+        if (arr[i] < arr[i - 1]) {
+            R = i;
+            break;
+        }
     }
-    printf("\n");
+    int minVal = arr[L], maxVal = arr[L];
+    for (int i = L; i <= R; i++) {
+        if (arr[i] < minVal) minVal = arr[i];
+        if (arr[i] > maxVal) maxVal = arr[i];
+    }
+    for (int i = 0; i < L; i++) {
+        if (arr[i] > minVal) {
+            L = i;
+            break;
+        }
+    }
+    for (int i = n - 1; i > R; i--) {
+        if (arr[i] < maxVal) {
+            R = i;
+            break;
+        }
+    }
+
+    return R - L + 1;
 }
